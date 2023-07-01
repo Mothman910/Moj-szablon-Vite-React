b0271390f76df82eee10dfe05c716edcc9f62f9f
Adam Fijałkowski <fijalkowski.adam@outlook.com>
2023-07-01 20:01:22 +0200

    Moja maszyna losująca

--- src/js/App/QuoteMachine.jsx
+++ src/js/App/QuoteMachine.jsx
@@ -38,7 +38,7 @@ export default function QuoteMachine() {
 
   return (
     <div className="row justify-content-center align-items-center">
-      <div className="card text-center">
+      <div className="card text-center quote-machine">
         <div className="card-header">Random Quote Machine</div>
         <div className="card-body">
           <h5 id="author" className="card-title">
@@ -47,15 +47,36 @@ export default function QuoteMachine() {
           <p id="text" className="card-text">
             {`${quotes[currentIndex].quote}`}
           </p>
-          <button
-            onClick={handleClick}
-            id="new-quote"
-            href="#"
-            className="btn btn-primary">
-            Następny cytat
-          </button>
+          <div className="buttons row justify-content-between justify-content-sm-start gap-3 position-relative">
+            <a
+              className="btn button position-relative"
+              id="share-quote"
+              title="Udostępnij ten cytat!"
+              target="_blank"
+              href="https://www.facebook.com/sharer.php?u=http://localhost:8080"
+              style={{ backgroundColor: "rgb(39, 174, 96)" }}>
+              <i className="fa fa-facebook"></i>
+            </a>
+            <a
+              className="btn button position-relative"
+              id="tweet-quote"
+              title="Tweet this quote!"
+              target="_blank"
+              href={`https://twitter.com/intent/tweet?hashtags=quotes&amp;related=mothman910&amp;text=${quotes[currentIndex].quote}`}>
+              <i class="fa fa-twitter"></i>
+            </a>
+            <div className="row position-absolute justify-content-center button-row">
+              <button
+                onClick={handleClick}
+                id="new-quote"
+                href="#"
+                className="btn btn-primary col-7 col-sm-4 col-lg-4">
+                Następny cytat
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="card-footer text-body-secondary">{nextRefresh}</div>
+        <div className="card-footer">{nextRefresh}</div>
       </div>
     </div>
   );
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -7,9 +7,6 @@ import { createRoot } from "react-dom/client";
 
 // Import Components
 import QuoteMachine from "./App/QuoteMachine.jsx";
-// function QuoteMachine() {
-//   return <h1>Blabla</h1>;
-// }
 
 const root = createRoot(document.getElementById("quote-box"));
 root.render(
