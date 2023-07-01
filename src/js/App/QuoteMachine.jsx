eaa1cd1b9722e77c36419f1bb5db5ea8dee64e5a
Adam Fijałkowski <fijalkowski.adam@outlook.com>
2023-07-01 16:31:37 +0200

    Moja maszyna do losowania cytatów

--- src/js/App/QuoteMachine.jsx
+++ src/js/App/QuoteMachine.jsx
@@ -1,23 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import quotes from "../data/quotes.json";
+import colorsPalette from "../data/colors-palette.json";
+import imagesTitles from "../data/images-titles.json";
 
 export default function QuoteMachine() {
-  console.log(quotes);
+  const timeToNextQuote = 10;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [nextRefresh, setNextRefresh] = useState(timeToNextQuote);
+
+  const handleClick = () => {
+    setCurrentIndex((prevIndex) => {
+      return prevIndex === quotes.length - 1 ? 0 : prevIndex + 1;
+    });
+    document.body.style.backgroundImage = `url("../images/${
+      imagesTitles[Math.round(Math.random() * imagesTitles.length)]
+    }")`;
+    // document.body.style.backgroundColor =
+    //   colorsPalette[Math.round(Math.random() * colorsPalette.length)];
+    setNextRefresh(timeToNextQuote);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNextRefresh((prevTime) => {
+        if (prevTime === 1) {
+          handleClick();
+        } else {
+          return prevTime - 1;
+        }
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className="row justify-content-center align-items-center">
       <div className="card text-center">
-        <div className="card-header">Featured</div>
+        <div className="card-header">Random Quote Machine</div>
         <div className="card-body">
-          <h5 className="card-title">Special title treatment</h5>
-          <p className="card-text">
-            With supporting text below as a natural lead-in to additional
-            content.
+          <h5 id="author" className="card-title">
+            {`${quotes[currentIndex].author}`}
+          </h5>
+          <p id="text" className="card-text">
+            {`${quotes[currentIndex].quote}`}
           </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+          <button
+            onClick={handleClick}
+            id="new-quote"
+            href="#"
+            className="btn btn-primary">
+            Następny cytat
+          </button>
         </div>
-        <div className="card-footer text-body-secondary">2 days ago</div>
+        <div className="card-footer text-body-secondary">{nextRefresh}</div>
       </div>
     </div>
   );
