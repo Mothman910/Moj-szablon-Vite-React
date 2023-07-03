ce8a3441aec65456f0241db286830fe262fa6aac
Adam Fijałkowski <fijalkowski.adam@outlook.com>
2023-07-03 16:57:49 +0200

    start

--- src/js/App/DrumMachine.jsx
+++ src/js/App/DrumMachine.jsx
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from "react";
 import quotes from "../data/quotes.json";
 import colorsPalette from "../data/colors-palette.json";
 
-export default function QuoteMachine() {
-  const timeToNextQuote = 10;
+export default function DrumMachine() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [nextRefresh, setNextRefresh] = useState(timeToNextQuote);
 
